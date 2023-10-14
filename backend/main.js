3707cffe15543f490a1784977a29284c54c2c551
Melhem Fares <faresm0305@gmail.com>
2023-10-13 22:35:19 -0400

    setting up backend

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Use the provided port or 3000 as a default
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, Express.js!');
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
